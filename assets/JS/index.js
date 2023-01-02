3b54dd774eb54f34f25f78448994064836952708
kamaleshfssa <118255046+kamaleshfssa@users.noreply.github.com>
2023-01-02 15:26:27 +0530

    Responsive 1

---
+++ assets/JS/index.js
@@ -0,0 +1,114 @@
+function nextusers() {
+  document.getElementById("uimg1").style.display = "none";
+  document.getElementById("uimg2").style.display = "none";
+  document.getElementById("uimg3").style.display = "none";
+  document.getElementById("uimg4").style.display = "none";
+  document.getElementById("uimg5").style.display = "block";
+  document.getElementById("uimg6").style.display = "block";
+  document.getElementById("uimg7").style.display = "block";
+  document.getElementById("uimg8").style.display = "block";
+  document.getElementById("uimg9").style.display = "block";
+  document.getElementById("back").style.display = "block";
+  document.getElementById("unext").style.display = "none";
+  document.getElementById;
+}
+
+function backusers() {
+  document.getElementById("uimg1").style.display = "block";
+  document.getElementById("uimg2").style.display = "block";
+  document.getElementById("uimg3").style.display = "block";
+  document.getElementById("uimg4").style.display = "block";
+  document.getElementById("uimg5").style.display = "block";
+  document.getElementById("uimg6").style.display = "none";
+  document.getElementById("uimg7").style.display = "none";
+  document.getElementById("uimg8").style.display = "none";
+  document.getElementById("uimg9").style.display = "none";
+  document.getElementById("back").style.display = "none";
+  document.getElementById("unext").style.display = "block";
+}
+
+function TPmore() {
+  document.getElementById("TP15").style.display = "flex";
+  document.getElementById("TP14").style.display = "flex";
+  document.getElementById("TP13").style.display = "flex";
+  document.getElementById("TP12").style.display = "flex";
+  document.getElementById("TP11").style.display = "flex";
+  document.getElementById("TP10").style.display = "flex";
+  document.getElementById("TP9").style.display = "flex";
+  document.getElementById("TP8").style.display = "flex";
+  document.getElementById("TP6").style.display = "flex";
+  document.getElementById("more").style.display = "none";
+  document.getElementById("less").style.display = "block";
+}
+
+function TPless() {
+  document.getElementById("TP15").style.display = "none";
+  document.getElementById("TP14").style.display = "none";
+  document.getElementById("TP13").style.display = "none";
+  document.getElementById("TP12").style.display = "none";
+  document.getElementById("TP11").style.display = "none";
+  document.getElementById("TP10").style.display = "none";
+  document.getElementById("TP9").style.display = "none";
+  document.getElementById("TP8").style.display = "none";
+  document.getElementById("TP6").style.display = "none";
+  document.getElementById("more").style.display = "block";
+  document.getElementById("less").style.display = "none";
+}
+
+function TrendMore() {
+  document.getElementById("trend10").style.display = "block";
+  document.getElementById("trend9").style.display = "block";
+  document.getElementById("trend8").style.display = "block";
+  document.getElementById("trend7").style.display = "block";
+  document.getElementById("trend6").style.display = "block";
+  document.getElementById("trendmore").style.display = "none";
+  document.getElementById("trendless").style.display = "block";
+}
+
+function TrendLess() {
+  document.getElementById("trend10").style.display = "none";
+  document.getElementById("trend9").style.display = "none";
+  document.getElementById("trend8").style.display = "none";
+  document.getElementById("trend7").style.display = "none";
+  document.getElementById("trend6").style.display = "none";
+  document.getElementById("trendmore").style.display = "block";
+  document.getElementById("trendless").style.display = "none";
+}
+
+function TPJMore() {
+  document.getElementById("TPJ18").style.display = "block";
+  document.getElementById("TPJ17").style.display = "block";
+  document.getElementById("TPJ16").style.display = "block";
+  document.getElementById("TPJ15").style.display = "block";
+  document.getElementById("TPJ14").style.display = "block";
+  document.getElementById("TPJ13").style.display = "block";
+  document.getElementById("TPJ12").style.display = "block";
+  document.getElementById("TPJ11").style.display = "block";
+  document.getElementById("TPJ10").style.display = "block";
+  document.getElementById("TPJ9").style.display = "block";
+  document.getElementById("TPJ8").style.display = "block";
+  document.getElementById("TPJ7").style.display = "block";
+  document.getElementById("TPJ6").style.display = "block";
+  document.getElementById("TPJ5").style.display = "block";
+  document.getElementById("TPJMore").style.display = "none";
+  document.getElementById("TPJLess").style.display = "block";
+}
+
+function TPJLess() {
+  document.getElementById("TPJ18").style.display = "none";
+  document.getElementById("TPJ17").style.display = "none";
+  document.getElementById("TPJ16").style.display = "none";
+  document.getElementById("TPJ15").style.display = "none";
+  document.getElementById("TPJ14").style.display = "none";
+  document.getElementById("TPJ13").style.display = "none";
+  document.getElementById("TPJ12").style.display = "none";
+  document.getElementById("TPJ11").style.display = "none";
+  document.getElementById("TPJ10").style.display = "none";
+  document.getElementById("TPJ9").style.display = "none";
+  document.getElementById("TPJ8").style.display = "none";
+  document.getElementById("TPJ7").style.display = "none";
+  document.getElementById("TPJ6").style.display = "none";
+  document.getElementById("TPJ5").style.display = "none";
+  document.getElementById("TPJMore").style.display = "block";
+  document.getElementById("TPJLess").style.display = "none";
+}
