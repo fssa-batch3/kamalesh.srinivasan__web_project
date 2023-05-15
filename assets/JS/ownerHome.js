60c1149f95009165d7a189add2e7f6f3c2fd93e9
kamaleshfssa <118255046+kamaleshfssa@users.noreply.github.com>
2023-05-15 21:06:21 +0530

    feat: ui changes

--- assets/JS/ownerHome.js
+++ assets/JS/ownerHome.js
@@ -78,101 +78,163 @@ function createJobCard(jobs, i, appendTag) {
       mergedReg = ownerReg;
     }
 
-    let bio = JSON.parse(localStorage.getItem("BIO"));
+    // let bio = JSON.parse(localStorage.getItem("BIO"));
 
-    let rect_cards = document.createElement("div");
-    rect_cards.setAttribute("class", "rect_cards");
+    // let rect_cards = document.createElement("div");
+    // rect_cards.setAttribute("class", "rect_cards");
 
-    let ED = document.createElement("div");
-    ED.setAttribute("class", "ED");
+    // let ED = document.createElement("div");
+    // ED.setAttribute("class", "ED");
 
-    let editA = document.createElement("a");
-    editA.setAttribute("href", `EditYourJob.html?id= ${jobs[i]["id"]}`);
-    ED.append(editA);
+    // let editA = document.createElement("a");
+    // editA.setAttribute("href", `EditYourJob.html?id= ${jobs[i]["id"]}`);
+    // ED.append(editA);
 
-    let editImg = document.createElement("img");
-    editImg.setAttribute("class", "editImg");
-    editImg.setAttribute("alt", "Image");
-    editImg.setAttribute("src", "../assets/Images/edit.svg");
-    editA.append(editImg);
+    // let editImg = document.createElement("img");
+    // editImg.setAttribute("class", "editImg");
+    // editImg.setAttribute("alt", "Image");
+    // editImg.setAttribute("src", "../assets/Images/edit.svg");
+    // editA.append(editImg);
 
-    let jobId = document.createElement("p");
-    jobId.innerText = `${jobs[i]["id"]}`;
-    ED.append(jobId);
+    // let jobId = document.createElement("p");
+    // jobId.innerText = `${jobs[i]["id"]}`;
+    // ED.append(jobId);
 
-    let deleteImg = document.createElement("img");
-    deleteImg.setAttribute("src", "../assets/Images/delete.svg");
-    deleteImg.setAttribute("alt", "Image");
-    deleteImg.setAttribute("id", "btn");
-    ED.append(deleteImg);
-    rect_cards.append(ED);
+    // let deleteImg = document.createElement("img");
+    // deleteImg.setAttribute("src", "../assets/Images/delete.svg");
+    // deleteImg.setAttribute("alt", "Image");
+    // deleteImg.setAttribute("id", "btn");
+    // ED.append(deleteImg);
+    // rect_cards.append(ED);
 
-    let secondCard = document.createElement("div");
-    secondCard.setAttribute("class", "secondCard");
+    // let secondCard = document.createElement("div");
+    // secondCard.setAttribute("class", "secondCard");
 
-    let profileData = profileImg(jobs[i]["FN"], jobs[i]["LN"]);
-    let profileImg1 = document.createElement("img");
-    profileImg1.setAttribute("src", profileData);
-    profileImg1.setAttribute("alt", "Profile");
-    secondCard.append(profileImg1);
+    // let profileData = profileImg(jobs[i]["FN"], jobs[i]["LN"]);
+    // let profileImg1 = document.createElement("img");
+    // profileImg1.setAttribute("src", profileData);
+    // profileImg1.setAttribute("alt", "Profile");
+    // secondCard.append(profileImg1);
 
-    //  Get the job created owner name
+    // //  Get the job created owner name
 
-    let cntOwner = mergedReg.find((F) => F.id == jobs[i]["ownerId"]);
-    let cntOwnerBio = bio.find((F) => F.Email == cntOwner.Email);
-    console.log(cntOwner);
+    // let cntOwner = mergedReg.find((F) => F.id == jobs[i]["ownerId"]);
+    // let cntOwnerBio = bio.find((F) => F.Email == cntOwner.Email);
+    // console.log(cntOwner);
 
-    let Prof_name = document.createElement("p");
-    Prof_name.innerText = cntOwnerBio["FN"] + cntOwnerBio["LN"];
-    Prof_name.setAttribute("class", "Prof_name");
-    secondCard.append(Prof_name);
+    // let Prof_name = document.createElement("p");
+    // Prof_name.innerText = cntOwnerBio["FN"] + cntOwnerBio["LN"];
+    // Prof_name.setAttribute("class", "Prof_name");
+    // secondCard.append(Prof_name);
 
-    let TypeP = document.createElement("p");
-    TypeP.setAttribute("class", "Type");
-    TypeP.innerText = `${jobs[i]["title"]}`;
+    // let TypeP = document.createElement("p");
+    // TypeP.setAttribute("class", "Type");
+    // TypeP.innerText = `${jobs[i]["title"]}`;
 
-    let TypeSpan = document.createElement("span");
-    TypeSpan.innerText = "Title :";
-    TypeP.prepend(TypeSpan);
+    // let TypeSpan = document.createElement("span");
+    // TypeSpan.innerText = "Title :";
+    // TypeP.prepend(TypeSpan);
 
-    secondCard.append(TypeP);
+    // secondCard.append(TypeP);
 
-    let LocationP = document.createElement("p");
-    LocationP.setAttribute("class", "NOE");
-    LocationP.innerText = `${jobs[i]["location"]}`;
+    // let LocationP = document.createElement("p");
+    // LocationP.setAttribute("class", "NOE");
+    // LocationP.innerText = `${jobs[i]["location"]}`;
 
-    let LocationSpan = document.createElement("span");
-    LocationSpan.innerText = "Location :";
-    LocationP.prepend(LocationSpan);
+    // let LocationSpan = document.createElement("span");
+    // LocationSpan.innerText = "Location :";
+    // LocationP.prepend(LocationSpan);
 
-    secondCard.append(LocationP);
+    // secondCard.append(LocationP);
 
-    let uploaded_dateP = document.createElement("p");
-    uploaded_dateP.setAttribute("class", "uploaded_date");
-    uploaded_dateP.innerText = `${jobs[i]["Salary"]}`;
+    // let uploaded_dateP = document.createElement("p");
+    // uploaded_dateP.setAttribute("class", "uploaded_date");
+    // uploaded_dateP.innerText = `${jobs[i]["Salary"]}`;
 
-    let uploaded_dateSpan = document.createElement("span");
-    uploaded_dateSpan.innerText = "Salary :";
-    uploaded_dateP.prepend(uploaded_dateSpan);
+    // let uploaded_dateSpan = document.createElement("span");
+    // uploaded_dateSpan.innerText = "Salary :";
+    // uploaded_dateP.prepend(uploaded_dateSpan);
 
-    secondCard.append(uploaded_dateP);
+    // secondCard.append(uploaded_dateP);
 
-    let rectButtons = document.createElement("div");
-    rectButtons.setAttribute("class", "rect-buttons");
-    secondCard.append(rectButtons);
+    // let rectButtons = document.createElement("div");
+    // rectButtons.setAttribute("class", "rect-buttons");
+    // secondCard.append(rectButtons);
 
-    let mac = document.createElement("button");
-    mac.setAttribute("id", "MAC");
-    mac.setAttribute("onclick", `marckAsDelete(${jobs[i]["id"]})`);
-    mac.setAttribute("class", "ck_work");
-    mac.innerHTML = "Mark as completed";
-    rectButtons.append(mac);
+    // let mac = document.createElement("button");
+    // mac.setAttribute("id", "MAC");
+    // mac.setAttribute("onclick", `marckAsDelete(${jobs[i]["id"]})`);
+    // mac.setAttribute("class", "ck_work");
+    // mac.innerHTML = "Mark as completed";
+    // rectButtons.append(mac);
 
-    rect_cards.append(secondCard);
+    // rect_cards.append(secondCard);
 
-    appendTag.append(rect_cards);
+    // appendTag.append(rect_cards);
+
+    // new card
+
+    let card = document.createElement("div");
+    card.setAttribute("class", "card");
+
+    let top = document.createElement("div");
+    top.setAttribute("class", "top");
+    card.append(top);
+
+    let topLeft = document.createElement("div");
+    topLeft.setAttribute("class", "topLeft");
+    topLeft.setAttribute("onclick", `marckAsDelete(${jobs[i]["id"]})`);
+    top.append(topLeft);
+
+    let circle = document.createElement("div");
+    circle.setAttribute("class", "circle");
+    circle.innerHTML = `<i class="fa fa-check" style="color: black;"></i> <div class="tooltiptext">Merk as completed</div>`;
+    topLeft.append(circle);
+
+    let profileImge = profileImg(jobs[i]["FN"], jobs[i]["LN"]);
+
+    let topRight = document.createElement("div");
+    topRight.setAttribute("class", "topRight");
+    topRight.innerHTML = `<img src=${profileImge} alt="imge"><div class="name">${
+      jobs[i]["FN"] + " " + jobs[i]["LN"]
+    }</div>`;
+    top.append(topRight);
+
+    let middle = document.createElement("div");
+    middle.setAttribute("class", "middle");
+    middle.innerHTML = `<div class="jobTitle">
+    ${jobs[i]["title"]}
+</div>
+<div class="description"> ${jobs[i]["disc"]}</div>`;
+    card.append(middle);
+
+    let bottom = document.createElement("div");
+    bottom.setAttribute("class", "bottom");
+    card.append(bottom);
+
+    let buttons = document.createElement("div");
+    buttons.setAttribute("class", "buttons");
+    bottom.append(buttons);
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.setAttribute("class", "delete");
+    deleteBtn.innerHTML = `<i class="fa fa-trash" style="color: black;"></i> <div class="tooltiptext">Delete</div>`;
+    buttons.append(deleteBtn);
+
+    let edit = document.createElement("button");
+    edit.setAttribute("class", "edit");
+    edit.setAttribute("onclick", `editojob(${jobs[i]["id"]})`);
+    edit.innerHTML = `<i class="fa fa-edit" style="color: black;"></i> <div class="tooltiptext">Edit</div>`;
+    buttons.append(edit);
+
+    let amount = document.createElement("div");
+    amount.setAttribute("class", "amount");
+    amount.innerHTML = `RS.${jobs[i]["Salary"]}`;
+    bottom.append(amount);
+
     // To to add a addEventlistener in the delete button
-    deleteImg.addEventListener("click", () => {
+
+    deleteBtn.addEventListener("click", () => {
       let text = "Are you sure to delete this job";
       if (confirm(text) == true) {
         const indexOfThis = jobs.indexOf(jobs[i]);
@@ -187,6 +249,8 @@ function createJobCard(jobs, i, appendTag) {
         });
       }
     });
+
+    document.querySelector(".cards").append(card);
   } catch (err) {
     console.error(err);
   }
@@ -207,7 +271,7 @@ function marckAsDelete(id) {
     }
     localStorage.setItem("requirements", JSON.stringify(getLocal));
     console.log(getLocal);
-    location.reload();
+    // location.reload();
   } catch (err) {
     console.error(err);
   }
@@ -243,4 +307,16 @@ function chatRD() {
   } catch (err) {
     console.error(err);
   }
+}
+
+function editojob(id) {
+  location.href = `EditYourJob.html?id=${id}`;
+}
+
+function openCreateJob() {
+  document.querySelector(".form").style.display = "flex";
+}
+
+function closeCreateJob() {
+  document.querySelector(".form").style.display = "none";
 }
--- assets/JS/register.js
+++ assets/JS/register.js
@@ -1,9 +1,11 @@
 function sendMail(OTP) {
   try {
+    let email = document.getElementById("email").value.toLowerCase().split("@");
+
     let params = {
       name: "kamalesh",
       email: document.getElementById("email").value.toLowerCase(),
-      to_name: document.getElementById("FullName").value,
+      to_name: email[0],
       code: OTP,
     };
 
@@ -14,7 +16,7 @@ function sendMail(OTP) {
       .send(serviceID, templateID, params)
       .then((res) => {
         console.log(res);
-        alert("Your message sent successfully!!");
+        // alert("Your message sent successfully!!");
       })
       .catch((err) => console.log(err));
   } catch (err) {
@@ -53,4 +55,42 @@ function encryptPassword(password) {
     console.error("Error encrypting password:", error);
     throw error;
   }
+}
+
+function passwordEye() {
+  document.getElementById("password").setAttribute("type", "text");
+  document
+    .getElementById("passwordEye")
+    .setAttribute("class", "fa fa-eye-slash");
+  document
+    .getElementById("passwordEye")
+    .setAttribute("onclick", "passwordEyeCross()");
+}
+
+function passwordEyeCross() {
+  document.getElementById("password").setAttribute("type", "password");
+  document.getElementById("passwordEye").setAttribute("class", "fa fa-eye");
+  document
+    .getElementById("passwordEye")
+    .setAttribute("onclick", "passwordEye()");
+}
+
+function RepasswordEye() {
+  document.getElementById("repeatPassword").setAttribute("type", "text");
+  document
+    .getElementById("repeatPasswordEye")
+    .setAttribute("class", "fa fa-eye-slash");
+  document
+    .getElementById("repeatPasswordEye")
+    .setAttribute("onclick", "RepasswordEyeCross()");
+}
+
+function RepasswordEyeCross() {
+  document.getElementById("repeatPassword").setAttribute("type", "password");
+  document
+    .getElementById("repeatPasswordEye")
+    .setAttribute("class", "fa fa-eye");
+  document
+    .getElementById("repeatPasswordEye")
+    .setAttribute("onclick", "RepasswordEye()");
 }
